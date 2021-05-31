8e96e391d0d0957c910960e9650da0e6249c7b50
jx2lee <jaejun.lee.1991@gmail.com>
2021-05-31 19:15:54 +0900

    from 준영속 상태

--- src/main/java/com/jx2lee/springjpa/JpaMain.java
+++ src/main/java/com/jx2lee/springjpa/JpaMain.java
@@ -14,14 +14,31 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
+            // 준영속상태
+            // JPA 에서 더이상 관리하지 않겠다는 의미 (detach)
+            // 주로 사용하지 않지만 확인용으로 사용할 수 있으니 사용 방법은 익혀두도록 하자.
+            Member member = em.find(Member.class, 25L);
+            member.setName("aaaaaaa");
 
-//            // insert query 는 persist 이후 쓰기 지연 SQL 저장소에 담겨져 있는 SQL 들이 모두 실행된다.
-//            // insert query 는 모두 == 이후에 발생하는 것을 확인할 수 있다.
-            em.persist(member1);
-            em.persist(member2);
-            System.out.println("============================");
+            em.detach(member);
+
+            // flush
+            // em.flush 을 수행하면 쓰기 지연 SQL 저장소의 변경 쿼리가 DB 로 전달하여 수행한다.
+            // DB 에 쓰기(수정, 삭제) 작업이 진행된다.
+            // 영속성 컨텍스트를 비우지 않고 영속성 컨텍스트의 변경내용을 데이터베이스에 동기화
+//            Member member = new Member(25L, "test");
+//            em.persist(member);
+//
+//            em.flush();
+//            System.out.println("============================");
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+            // commit 이후 쓰기 지연 SQL 저장소에 담겨져 있는 SQL 들이 모두 실행된다.
+            // 모두 == 이후에 발생하는 것을 확인할 수 있다.
+//            em.persist(member1);
+//            em.persist(member2);
+//            System.out.println("============================");
 
             // Member 객체는 비영속 상태
 //            Member member = new Member();
@@ -52,27 +69,27 @@ public class JpaMain {
 
 
             // Member 객체는 비영속 상태
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("jx2lee");
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("jx2lee");
+//
 
             // 영속 상태
-            System.out.println("==== BEFORE ====");
-            em.persist(member);
-            System.out.println("==== AFTER ====");
+//            System.out.println("==== BEFORE ====");
+//            em.persist(member);
+//            System.out.println("==== AFTER ====");
 
-            tx.commit();
-
-//            수정
+            // 수정
 //            em.persist 는 사용하지 않아도 됨
 //            Member findMember = em.find(Member.class, 1L);
 //            findMember.setName("jx2lee2");
 //            tx.commit();
 
-//            삭제
+            // 삭제
 //            Member findMember = em.find(Member.class, 1L);
 //            em.remove(findMember);
-//            tx.commit();
+
+            tx.commit();
 
         } catch (Exception e) {
             tx.rollback();
