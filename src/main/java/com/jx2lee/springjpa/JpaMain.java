5af273ff7100aa9eadbf6a3462a40353bfb1a1bb
jx2lee <jaejun.lee.1991@gmail.com>
2021-06-09 20:28:48 +0900

    섹션 7. 고급 매핑: Mapped Superclass - 매핑 정보 상속
    * 공통 속성들을 모아서 관리할 때 사용하는 annotation
    * 등록자, 수정자, 등록 및 수정일과 같은 정보를 일일이 entitty 에 추가할 때
    * 추상 클래스로 생성하며 entity 가 아닌 class 는 상속받지 못함

--- src/main/java/com/jx2lee/springjpa/JpaMain.java
+++ src/main/java/com/jx2lee/springjpa/JpaMain.java
@@ -1,11 +1,12 @@
 package com.jx2lee.springjpa;
 
-import com.jx2lee.springjpa.domain.Movie;
+import com.jx2lee.springjpa.domain.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,21 +17,17 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Movie movie = new Movie();
-            movie.setDirector("이재준");
-            movie.setActor("신지영");
-            movie.setName("바람과함께 사라지다");
-            movie.setPrice(10000);
-            movie.setStockQuantity(1);
+            Member member = new Member();
+            member.setName("jaejun");
+            member.setCreatedBy("lee");
+            member.setCreatedDate(LocalDateTime.now());
 
-            em.persist(movie);
+            em.persist(member);
             em.flush();
             em.clear();
 
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
-
             tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
         } finally {
