df43a0b1f4e91a454f170eada237b380d899f0e5
jx2lee <jaejun.lee.1991@gmail.com>
2021-06-14 23:41:31 +0900

    섹션 9. 값 타입: 값 타입과 불편 객체
    * 임베디드 타입의 경우 객체 공유 시 참조하기 때문에 공유를 절대 해선 안된다.
    * 객체를 불변하게 설정
    * 다양한 방법들이 있는데..
      * setter 없애고 새로운 객체를 생성한 후 다시 세팅
      * setter 의 public -> private
      * 객체 불변 방법에 대해서도 시간이 된다면 정리해보자

--- src/main/java/com/jx2lee/springjpa/JpaMain.java
+++ src/main/java/com/jx2lee/springjpa/JpaMain.java
@@ -1,9 +1,14 @@
 package com.jx2lee.springjpa;
 
+import com.jx2lee.springjpa.domain.Address;
+import com.jx2lee.springjpa.domain.Delivery;
+import com.jx2lee.springjpa.domain.Member;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.print.attribute.standard.Media;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,6 +19,15 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Address address = new Address("city", "street", "10000");
+            Delivery delivery = new Delivery();
+            delivery.setAddress(address);
+            em.persist(delivery);
+
+            Delivery delivery2 = new Delivery();
+            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
+            delivery2.setAddress(newAddress);
+            em.persist(delivery2);
 
             tx.commit();
 
--- src/main/java/com/jx2lee/springjpa/domain/Delivery.java
+++ src/main/java/com/jx2lee/springjpa/domain/Delivery.java
@@ -20,4 +20,35 @@ public class Delivery extends BaseEntity {
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public DeliveryStatus getDeliveryStatus() {
+        return deliveryStatus;
+    }
+
+    public void setDeliveryStatus(DeliveryStatus deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
